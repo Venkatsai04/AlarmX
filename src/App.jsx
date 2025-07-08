2227e0a3bda55a2b364e15e9584edc88b1e50609
venkat sai <saik87630@gmail.com>
2025-07-09 00:25:24 +0530

    fix: updated layout and added new alarm logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>AlarmX</div>
+    <div  className='flex items-center justify-center flex-col mt-4 gap-3'>
+      <div>AlarmX</div>
+      <button className='pt-1 pb-1 pl-2 pr-2 bg-amber-200'>Set</button>
+    </div>
   )
 }
 
