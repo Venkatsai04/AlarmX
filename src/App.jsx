b98eaa3e8c1282210cf33ccd810a356ee6191960
venkat sai <saik87630@gmail.com>
2025-07-09 23:52:16 +0530

    added time functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [Time, setTime] = useState(0)
+
+  const getCurrTime = () => {
+    const now = new Date()
+    const Hours = now.getHours().toString()
+    const Minutes = now.getMinutes().toString()
+    const Seconds = now.getSeconds().toString()
+
+    const timeNow = `${Hours.length == 2? Hours: '0' + Hours}:${Minutes.length == 2? Minutes: '0' + Minutes}:${Seconds.length == 2? Seconds: '0' + Seconds}`
+
+    return timeNow
+  }
+
+  setInterval(() => {
+    console.log(getCurrTime());
+    setTime(getCurrTime())
+  }, 1000);
+
   return (
-    <div  className='flex items-center justify-center flex-col mt-4 gap-3'>
-      <div>AlarmX</div>
-      <button className='pt-1 pb-1 pl-2 pr-2 bg-amber-200'>Set</button>
-    </div>
+    <>
+      <div className='flex items-center justify-center flex-col mt-4 gap-3'>
+        <div>AlarmX</div>
+        <span className='bold xl'>{Time}</span>
+        <button className='pt-1 pb-1 pl-2 pr-2 bg-amber-200'>Set</button>
+      </div>
+    </>
   )
 }
 
